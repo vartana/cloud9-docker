18ea426cae6fb0ba1e02e4810b703f6ec095a3e9
Vartan Arabyan <vartana@gmail.com>
2016-01-26 15:14:52 -0700

    Create config.js

---
+++ conf/config.js
@@ -0,0 +1,16 @@
+exports.GITHUB_CLIENT_ID = 'f4348dd233a9a4c8d25b';
+exports.GITHUB_CLIENT_SECRET = '9e1cf5e350fbd3a05a7b5451551091cf4915b2c9';
+
+// This allows everybody to sign up
+exports.PERMITTED_USERS = false;
+// This would only allow alice and bob to sign up
+//exports.PERMITTED_USERS = ['', 'bob'];
+
+// Add SSL Certificates to use Cloud9Hub over SSL
+//   (Cloud9 Workspaces will still be unsecured standard HTTP)
+//exports.SSL = {
+//  key: "/path/to/ssl.key",
+//  cert: "/path/to/ssl.pem"
+//};
+
+exports.BASE_URL = 'http://c9.vartancode.com';
